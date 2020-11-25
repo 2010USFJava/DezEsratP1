371065b40062e29bf84694b5ce6e0320110d3519
esratjahan12 <esratjahan444@gmail.com>
2020-11-25 16:48:36 -0500

    Add beans

---
+++ DezEsratTRMS/src/main/java/com/revature/beans/Employee.java
@@ -0,0 +1,113 @@
+package com.revature.beans;
+
+public class Employee {
+	private int empID;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String email;
+	private String phone;
+	private String userName;
+	private String passWord;
+	private String empType;
+	
+	public Employee() {
+		
+	}
+
+	public Employee(int empID, String firstName, String lastName, String address, String email, String phone,
+			String userName, String passWord, String empType) {
+		super();
+		this.empID = empID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.email = email;
+		this.phone = phone;
+		this.userName = userName;
+		this.passWord = passWord;
+		this.empType = empType;
+	}
+
+	public int getEmpID() {
+		return empID;
+	}
+
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassWord() {
+		return passWord;
+	}
+
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+
+	public String getEmpType() {
+		return empType;
+	}
+
+	public void setEmpType(String empType) {
+		this.empType = empType;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empID=" + empID + ", firstName=" + firstName + ", lastName=" + lastName + ", address="
+				+ address + ", email=" + email + ", phone=" + phone + ", userName=" + userName + ", passWord="
+				+ passWord + ", empType=" + empType + "]";
+	}
+	
+	
+
+}
---
+++ DezEsratTRMS/src/main/java/com/revature/beans/Form.java
@@ -0,0 +1,134 @@
+package com.revature.beans;
+
+public class Form {
+	private int formID;
+	private int empID;
+	private String eventDate; 
+	private String reqDate;
+	private String location;
+	private int reqAmount;
+	private String eventDescription;
+	private String status;
+	private int eventID;
+	private int gradeID;
+	private int finalGrade;
+	
+	public Form() {
+		
+	}
+
+	public Form(int formID, int empID, String eventDate, String reqDate, String location, int reqAmount,
+			String eventDescription, String status, int eventID, int gradeID, int finalGrade) {
+		super();
+		this.formID = formID;
+		this.empID = empID;
+		this.eventDate = eventDate;
+		this.reqDate = reqDate;
+		this.location = location;
+		this.reqAmount = reqAmount;
+		this.eventDescription = eventDescription;
+		this.status = status;
+		this.eventID = eventID;
+		this.gradeID = gradeID;
+		this.finalGrade = finalGrade;
+	}
+
+	public int getFormID() {
+		return formID;
+	}
+
+	public void setFormID(int formID) {
+		this.formID = formID;
+	}
+
+	public int getEmpID() {
+		return empID;
+	}
+
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+
+	public String getEventDate() {
+		return eventDate;
+	}
+
+	public void setEventDate(String eventDate) {
+		this.eventDate = eventDate;
+	}
+
+	public String getReqDate() {
+		return reqDate;
+	}
+
+	public void setReqDate(String reqDate) {
+		this.reqDate = reqDate;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public int getReqAmount() {
+		return reqAmount;
+	}
+
+	public void setReqAmount(int reqAmount) {
+		this.reqAmount = reqAmount;
+	}
+
+	public String getEventDescription() {
+		return eventDescription;
+	}
+
+	public void setEventDescription(String eventDescription) {
+		this.eventDescription = eventDescription;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getEventID() {
+		return eventID;
+	}
+
+	public void setEventID(int eventID) {
+		this.eventID = eventID;
+	}
+
+	public int getGradeID() {
+		return gradeID;
+	}
+
+	public void setGradeID(int gradeID) {
+		this.gradeID = gradeID;
+	}
+
+	public int getFinalGrade() {
+		return finalGrade;
+	}
+
+	public void setFinalGrade(int finalGrade) {
+		this.finalGrade = finalGrade;
+	}
+
+	@Override
+	public String toString() {
+		return "Form [formID=" + formID + ", empID=" + empID + ", eventDate=" + eventDate + ", reqDate=" + reqDate
+				+ ", location=" + location + ", reqAmount=" + reqAmount + ", eventDescription=" + eventDescription
+				+ ", status=" + status + ", eventID=" + eventID + ", gradeID=" + gradeID + ", finalGrade=" + finalGrade
+				+ "]";
+	}
+	
+	
+
+}
